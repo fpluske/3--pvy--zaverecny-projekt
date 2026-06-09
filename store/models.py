b11fad796aacf5077dc2ae7172211f2ad1a17ae7
fpluske <frantisek.pluske@seznam.cz>
2026-06-09 11:34:56 +0200

    V 1.1

--- store/models.py
+++ store/models.py
@@ -38,7 +38,7 @@ class Product(models.Model):
 	price = models.DecimalField(max_digits=10, decimal_places=2)
 	stock = models.PositiveIntegerField(default=0)
 	featured = models.BooleanField(default=False)
-	image_url = models.URLField(blank=True)
+	image = models.ImageField(upload_to='products/', blank=True, null=True)
 
 	class Meta:
 		ordering = ['name']
